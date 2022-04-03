263d32dae8725b0a4bf2a0c6b16d32a286d6ac45
Sam Miller <sam@itsecureadmin.com>
2022-04-03 15:33:21 -0700

    add db context template

--- AspTemplate/Areas/Identity/Data/AspTemplateContext.cs
+++ AspTemplate/Areas/Identity/Data/AspTemplateContext.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using AspTemplate.Areas.Identity.Data;
+using AspTemplate.Models;
 
 namespace AspTemplate.Areas.Identity.Data
 {
-	public class AspTemplateContext
+	public class AspTemplateContext : IdentityDbContext<AspTemplateUser>
 	{
-		
-	}
+        public AspTemplateContext(DbContextOptions<AspTemplateContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
 }
 

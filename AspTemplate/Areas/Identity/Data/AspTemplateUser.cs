217d5b5e7199cc765a8bebf2cc4577fc37362267
Sam Miller <sam@itsecureadmin.com>
2022-04-03 15:27:58 -0700

    add db context template

--- AspTemplate/Areas/Identity/Data/AspTemplateUser.cs
+++ AspTemplate/Areas/Identity/Data/AspTemplateUser.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspTemplate.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace AspTemplate.Areas.Identity.Data
 {
-	public class AspTemplateUser
+	// Add custom data fields to the User object by adding properties to this class
+	public class AspTemplateUser : IdentityUser
 	{
 		
 	}
